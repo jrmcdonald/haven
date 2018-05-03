0a011edad01db775c1e857737743a96c74e2d6c4
Jamie McDonald <jamie@qwyck.net>
2018-05-03 15:29:27 +0100

    Initial pass at party achievements.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class PartyDetails extends Component {
     super();
 
     this.mapReputationOptions = this.mapReputationOptions.bind(this);
+    this.mapAchievements = this.mapAchievements.bind(this);
 
     this.updateLocalStorage = this.updateLocalStorage.bind(this);
 
@@ -17,6 +18,10 @@ class PartyDetails extends Component {
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleNotesChange = this.handleNotesChange.bind(this);
+
+    this.handleAchievementInputChange = this.handleAchievementInputChange.bind(this);
+    this.handleAchievementAdd = this.handleAchievementAdd.bind(this);
+    this.handleAchievementDelete = this.handleAchievementDelete.bind(this);
 
     this.state = {
       defaults: {
@@ -80,13 +85,18 @@ class PartyDetails extends Component {
   }
 
   mapReputationOptions = item => ( <option key={item.key} value={item.key}>{item.key}</option> );
+  mapAchievements = item => ( <li className="list-group-item clearfix" key={item}>{item}<span className="float-right"><span className="btn btn-xs btn-default" onClick={() => this.handleAchievementDelete(item)}><i class="far fa-trash-alt" aria-hidden="true"></i></span></span></li> );
 
   updateLocalStorage = () => { localStorage.setItem('party', JSON.stringify(this.state.party)) };
 
-  handleReputationChange = (event) => this.setState({ party: { ...this.state.party, reputation: event.target.value } }, () => this.updateLocalStorage());
-  handleNameChange = (event) => this.setState({ party: { ...this.state.party, name: event.target.value } }, () => this.updateLocalStorage());
-  handleLocationChange = (event) => this.setState({ party: { ...this.state.party, location: event.target.value } }, () => this.updateLocalStorage());
-  handleNotesChange = (event) => this.setState({ party: { ...this.state.party, notes: event.target.value } }, () => this.updateLocalStorage());
+  handleReputationChange = event => this.setState({ party: { ...this.state.party, reputation: event.target.value } }, () => this.updateLocalStorage());
+  handleNameChange = event => this.setState({ party: { ...this.state.party, name: event.target.value } }, () => this.updateLocalStorage());
+  handleLocationChange = event => this.setState({ party: { ...this.state.party, location: event.target.value } }, () => this.updateLocalStorage());
+  handleNotesChange = event => this.setState({ party: { ...this.state.party, notes: event.target.value } }, () => this.updateLocalStorage());
+
+  handleAchievementInputChange = event => this.setState({ achievement: event.target.value });
+  handleAchievementAdd = event => this.setState({ party: { ...this.state.party, achievements: this.state.party.achievements.concat([this.state.achievement]) } }, () => this.updateLocalStorage());
+  handleAchievementDelete = item => this.setState({ party: { ...this.state.party, achievements: this.state.party.achievements.filter(el => el != item) } }, () => this.updateLocalStorage());
 
   render() {
     return (
@@ -113,13 +123,29 @@ class PartyDetails extends Component {
               </div>
               <div className="form-group col-md-6">
                 <label htmlFor="modifier">Shop Price Modifier:</label>
-                <input type="text" name="modifier" id="modifier" className="form-control" disabled value={this.state.defaults.reputation.find(o => o.key === this.state.party.reputation).value} />
+                <input type="text" name="modifier" id="modifier" className="form-control" readonly value={this.state.defaults.reputation.find(o => o.key === this.state.party.reputation).value} />
               </div>
             </div>
             <div className="form-row">
               <div className="form-group col-md-12">
                 <label htmlFor="notes">Notes:</label>
                 <textarea name="notes" id="notes" className="form-control" value={this.state.party.notes} onChange={this.handleNotesChange} />
+              </div>
+            </div>
+            <div className="form-row align-items-top">
+              <div className="form-group col-md-5">
+                <input type="text" name="achievements" id="achievements" className="form-control" placeholder="What have you achieved?" value={this.state.achievement} onChange={this.handleAchievementInputChange} />
+              </div>
+              <div className="form-group col-md-1">
+                <input type="button" className="btn btn-info btn-block" name="add-achievement" id="add-achievement" value="Add" onClick={this.handleAchievementAdd} />
+              </div>
+              <div className="form-group col-md-6">
+                <div className="card">
+                  <div className="card-header">Party Achievements</div>
+                  <ul className="list-group list-group-flush">
+                    {this.state.party.achievements.map(this.mapAchievements)}
+                  </ul>
+                </div>
               </div>
             </div>
           </form>
